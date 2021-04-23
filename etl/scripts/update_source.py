96a9644c6a8b8738ac90c979c7ab50bd38289755
semio <semio@posteo.net>
2021-04-23 14:10:25 +0800

    fix path again

--- etl/scripts/update_source.py
+++ etl/scripts/update_source.py
@@ -4,7 +4,7 @@ from ddf_utils.factory.common import download
 
 # brackets data are downloaded from
 # https://docs.google.com/spreadsheets/d/1QQqbMj6yYclwB3Q9xLYVNUFaJskYoZEQKpzYHvlnuas/edit#gid=0
-brackets_csv = './brackets.csv'
+brackets_csv = '../source/brackets.csv'
 brackets_df = pd.read_csv(brackets_csv)
 all_brackets = brackets_df['bracket_start'].to_list()
 all_brackets.append(brackets_df['bracket_end'].iloc[-1])  # Append last value from bracket end
