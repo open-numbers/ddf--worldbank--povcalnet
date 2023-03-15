0724d461ca7b147af8c3cdaf97c2c1f4087d7228
semio <semio@posteo.net>
2023-03-15 14:32:44 +0800

    update source downloader
    now we will download 461 files from povcalnet

--- etl/scripts/update_source.py
+++ etl/scripts/update_source.py
@@ -1,3 +1,5 @@
+#%%
+import sys
 import os
 import os.path as osp
 import glob
@@ -7,21 +9,12 @@ from ddf_utils.factory.common import download
 from multiprocessing import Pool
 from functools import partial
 
-# brackets data are downloaded from
-# https://docs.google.com/spreadsheets/d/1QQqbMj6yYclwB3Q9xLYVNUFaJskYoZEQKpzYHvlnuas/edit#gid=0
-# brackets_csv = '../source/fixtures/brackets.csv'
-# brackets_df = pd.read_csv(brackets_csv)
-# all_brackets = brackets_df['bracket_start'].to_list()
-# all_brackets.append(
-#     brackets_df['bracket_end'].iloc[-1])  # Append last value from bracket end
-
-source_dir = '../source/income_mountain'
-all_brackets = np.logspace(-7, 13, 201, endpoint=True, base=2)
-# url_tmpl = "http://iresearch.worldbank.org/PovcalNet/PovcalNetAPI.ashx?YearSelected=all&PovertyLine={}&Countries=all&display=C&format=csv"
+#%%
+source_dir = 'source/povcalnet'
+all_brackets = np.logspace(-7, 13, 501, base=2, endpoint=True)
 url_tmpl = "https://api.worldbank.org/pip/v1/pip?country=all&year=all&povline={}&fill_gaps=true&group_by=none&welfare_type=all&reporting_level=all&format=csv"
 POOLSIZE = 5
-
-
+# %%
 def process(i, resume=True):
     bracket = all_brackets[i]
     url = url_tmpl.format(bracket)
@@ -34,14 +27,25 @@ def process(i, resume=True):
              backoff=5,
              timeout=60)
     time.sleep(5)
-
-
+# %%
 if __name__ == "__main__":
+    # create the folder
+    os.makedirs(source_dir, exist_ok=True)
+
     # remove all old files first
-    for f in glob.glob('../source/income_mountain/*.csv'):
-        os.remove(f)
-    run = partial(process, resume=False)
-    with Pool(POOLSIZE) as p:
-        r = range(len(all_brackets))
-        p.map(run, r[::-1])
-    print("done.")
+    # for f in glob.glob('../source/robin_hood_500/*.csv'):
+    #     os.remove(f)
+    if len(sys.argv) > 1:
+        bracket = int(sys.argv[1])
+        if bracket > 460:
+            print("do not download bracket > 460")
+            sys.exit(127)
+        print(f'downloading {bracket}')
+        process(bracket, resume=False)
+
+    # we have 461 groups to download
+    # brackets = all_brackets[all_brackets < 2705]
+    # run = partial(process, resume=False)
+    # with Pool(POOLSIZE) as p:
+    #     r = range(len(brackets))
+    #     p.map(run, r[::-1])
