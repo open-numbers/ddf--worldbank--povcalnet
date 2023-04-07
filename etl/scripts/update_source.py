3c9dc200537a40cc3cf1b79eebb7b63f8574e929
semio <semio@posteo.net>
2023-04-07 11:46:28 +0800

    update source downloading script

--- etl/scripts/update_source.py
+++ etl/scripts/update_source.py
@@ -1,4 +1,5 @@
-#%%
+# -*- coding: utf-8 -*-
+
 import sys
 import os
 import os.path as osp
@@ -9,12 +10,13 @@ from ddf_utils.factory.common import download
 from multiprocessing import Pool
 from functools import partial
 
-#%%
+
 source_dir = 'source/povcalnet'
 all_brackets = np.logspace(-7, 13, 501, base=2, endpoint=True)
 url_tmpl = "https://api.worldbank.org/pip/v1/pip?country=all&year=all&povline={}&fill_gaps=true&group_by=none&welfare_type=all&reporting_level=all&format=csv"
-POOLSIZE = 5
-# %%
+POOLSIZE = 4
+
+
 def process(i, resume=True):
     bracket = all_brackets[i]
     url = url_tmpl.format(bracket)
@@ -27,13 +29,14 @@ def process(i, resume=True):
              backoff=5,
              timeout=60)
     time.sleep(5)
-# %%
+
+
 if __name__ == "__main__":
     # create the folder
     os.makedirs(source_dir, exist_ok=True)
 
-    # remove all old files first
-    # for f in glob.glob('../source/robin_hood_500/*.csv'):
+    # uncomment this to remove all old files first
+    # for f in glob.glob('source/povcalnet/*.csv'):
     #     os.remove(f)
     if len(sys.argv) > 1:
         bracket = int(sys.argv[1])
@@ -43,9 +46,9 @@ if __name__ == "__main__":
         print(f'downloading {bracket}')
         process(bracket, resume=False)
 
-    # we have 461 groups to download
-    # brackets = all_brackets[all_brackets < 2705]
-    # run = partial(process, resume=False)
-    # with Pool(POOLSIZE) as p:
-    #     r = range(len(brackets))
-    #     p.map(run, r[::-1])
+    else:
+        brackets = all_brackets[all_brackets < 2705]  # 2705 is in group 461
+        run = partial(process, resume=False)
+        with Pool(POOLSIZE) as p:
+            r = range(len(brackets))
+            p.map(run, r[::-1])
