bb48fb2b1b4f49f4cdc5c050f40d3410e040a6a2
semio <semio@posteo.net>
2023-12-06 11:46:48 +0800

    add poverty rates for income groups

--- etl/scripts/extreme_poverty.py
+++ etl/scripts/extreme_poverty.py
@@ -3,6 +3,7 @@
 import os
 import numpy as np
 import polars as pl
+import pandas as pd
 import bracketlib
 
 data = pl.read_parquet('./bridged_shapes.parquet')
@@ -98,6 +99,82 @@ country_epov_rates = pl.from_records(country_epov)
 country_epov_rates
 
 country_epov_rates.write_csv('./ddf/poverty_rates/ddf--datapoints--poverty_rate--by--country--time.csv')
+
+# income groups
+# 1. get historical income groups
+wb_groups = pd.read_csv('../build/source/gapminder/wb_income_groups.csv')
+
+wb = wb_groups[['geo', 'time', "4 income groups (WB)"]].copy()
+
+wb.columns = ['geo', 'time', 'level']
+
+on_income = pd.read_csv('../build/source/gapminder/on_income_groups.csv')
+on_income = on_income[['geo', 'time', 'Income levels (GM)']].dropna()
+on_income.columns = ['geo', 'time', 'level']
+
+on_map = {'Level 1': 'l1',
+          'Level 2': 'l2',
+          'Level 3': 'l3',
+          'Level 4': 'l4',
+         }
+
+wb_map = {'Low income': 'l1',
+          'Lower middle income': 'l2',
+          'Upper middle income': 'l3',
+          'High income': 'l4',
+         }
+
+on = on_income.copy()
+on['level'] = on['level'].map(lambda x: on_map[x])
+wb['level'] = wb['level'].map(lambda x: wb_map[x])
+
+wb = wb.set_index(['geo', 'time'])
+on = on.set_index(['geo', 'time'])
+on.update(wb)
+
+income_groups = pl.DataFrame(on.reset_index()).select(
+    pl.col('geo').alias('country'),
+    pl.col('time').cast(pl.Int32).alias('year'),
+    pl.col('level')
+)
+
+# 2. get rates
+data_income_level = data.join(income_groups, on=['country', 'year'], how='left').group_by(
+    pl.col(['level', 'year', 'bracket'])
+).agg(
+    pl.col('population').sum()
+).sort(['level', 'year', 'bracket'])
+data_income_level
+
+data_income_level_list = data_income_level.partition_by(['level', 'year'], as_dict=True)
+
+level_epov = list()
+for k, _df in data_income_level_list.items():
+    level, y = k
+    level_epov.append({
+        'level': level,
+        'time': y,
+        'poverty_rate': get_epov_rates(_df)
+    })
+
+level_epov_rates = pl.from_records(level_epov)
+
+# level_epov_rates
+# level_epov_rates.filter(
+#     pl.col('time') == 2018
+# )
+# convert level to income groups
+concept_id_map = {'l1': 'low_income',
+                  'l2': 'lower_middle_income',
+                  'l3': 'upper_middle_income',
+                  'l4': 'high_income'
+                 }
+level_epov_rates = level_epov_rates.select(
+    pl.col('level').map_dict(concept_id_map).alias('income_groups'),
+    pl.col('time'),
+    pl.col('poverty_rate')
+)
+level_epov_rates.write_csv('./ddf/poverty_rates/ddf--datapoints--poverty_rate--by--income_groups--time.csv')
 
 # people in extreme poverty
 population = pl.read_csv('../build/source/gapminder/population.csv')
